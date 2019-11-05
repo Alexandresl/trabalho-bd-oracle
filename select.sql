311aba6e86e4f2dd17095912d73b55661a397cb6
alexandre <alexandre@teste.com.br>
2019-11-04 21:22:53 -0300

    final

--- select.sql
+++ select.sql
@@ -1,36 +1,45 @@
+/*1. Apresentar o nomes dos produtos em ordem crescente.*/
 SELECT nome 
 from PRODUTO 
 ORDER BY NOME ASC;
 
+/*2. Apresentar os nomes dos produtos, a quantidade em estoque e o 
+preço para os produtos que tem quantidade em estoque superior a 50 
+ou com preço inferior a 10.*/
 SELECT nome, quantidade_estoque, preco 
 from PRODUTO 
 WHERE quantidade_estoque > 50 
 OR (preco < 10);
 
+/*3. Apresentar o código do pedido, a data do pedido e o nome do cliente.*/
 SELECT pedido.id_pedido, pedido.data_pedido, cliente.nome 
 from pedido 
 INNER JOIN cliente 
 ON pedido.id_cliente = cliente.id_cliente;
 
+/*4. Apresentar o código do pedido, o código do produto, o nome do produto, 
+o preço de venda, ordenar pelo código do pedido.*/
 SELECT pedido_produto.id_pedido, pedido_produto.id_produto, produto.nome, produto.preco 
 FROM pedido_produto 
 INNER JOIN produto 
 ON pedido_produto.id_produto = produto.id_produto 
 ORDER BY PRODUTO.ID_PRODUTO ASC;
 
+/*5. Apresentar os nomes dos produtos que foram pedidos em quantidade superior a 10.*/
 SELECT PRODUTO.NOME 
 FROM PRODUTO 
 inner join PEDIDO_PRODUTO 
 ON PRODUTO.ID_PRODUTO = PEDIDO_PRODUTO.ID_PRODUTO 
 WHERE PEDIDO_PRODUTO.QUANTIDADE > 10;
 
+/*6. Apresentar o código do pedido, a data, o nome do cliente para os pedidos do produto de código 1.*/
 SELECT PEDIDO.ID_PEDIDO, PEDIDO.DATA_PEDIDO, CLIENTE.NOME
 FROM PEDIDO
 INNER JOIN CLIENTE ON PEDIDO.ID_CLIENTE = CLIENTE.ID_CLIENTE
 INNER JOIN PEDIDO_PRODUTO ON PEDIDO.ID_PEDIDO = PEDIDO_PRODUTO.ID_PEDIDO
 WHERE PEDIDO_PRODUTO.ID_PRODUTO = 1;
 
-/*Apresentar o código do pedido, a data, o nome do cliente para os pedidos do produto mesa.*/
+/*7. Apresentar o código do pedido, a data, o nome do cliente para os pedidos do produto mesa.*/
 
 SELECT PEDIDO.ID_PEDIDO, PEDIDO.DATA_PEDIDO, CLIENTE.NOME
 FROM PEDIDO
